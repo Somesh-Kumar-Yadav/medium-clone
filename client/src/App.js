61c30becbefda025ddae6981fc50e4f12853e437
Ransingh88 <rajaransingh@gmail.com>
2021-09-28 22:14:47 +0530

    code updated

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import { NavBar } from "./components/LandingPage/NavBar";
+import { WritePage } from "./pages/write/WritePage";
 
 function App() {
 	return (
 		<div className={styles.App}>
 			<NavBar />
+			<WritePage/>
 		</div>
 	);
 }
