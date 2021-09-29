0bb29b07bf89143887415ea30911fa2110495cbe
Manish Singh Bhadouria <60517957+ManishSinghBhadouria@users.noreply.github.com>
2021-09-30 00:14:51 +0530

    Atlas URL Added With LocalHost
    Atlas URL Added With LocalHost

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-	return mongoose.connect("mongodb://localhost:27017/medium", {
+	//LOCALHOST=>  mongodb://localhost:27017/medium
+	//ATLAS=>  mongodb+srv://medium:medium@cluster0.vpk1n.mongodb.net/mediumClone?retryWrites=true&w=majority
+	return mongoose.connect("mongodb+srv://medium:medium@cluster0.vpk1n.mongodb.net/mediumClone?retryWrites=true&w=majority", {
 		useCreateIndex: true,
 		useFindAndModify: true,
 		useNewUrlParser: true,
