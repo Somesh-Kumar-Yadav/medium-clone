d251435cef4dff2dcd1eb88a60f84c5aee1161e5
= <=>
2021-09-30 22:35:40 +0530

    Membership Page About to Complete

--- client/src/components/Membership/Membership.jsx
+++ client/src/components/Membership/Membership.jsx
@@ -13,6 +13,49 @@ const Membership = () => {
         </p>
         <button className={styles.btn1}>Subscribe today</button>
       </div>
+      <div className={styles.maindiv2}>
+        <div className={styles.innerdiv}>
+          <p className={styles.innerdivp1}>
+            Get unlimited access to every story.
+          </p>
+          <img
+            className={styles.innerdivimg}
+            src="https://cdn-static-1.medium.com/sites/medium.com/membership/images/UnlimitedReading.svg"
+            alt=""
+          />
+          <p className={styles.innerdivp2}>
+            Read any article in our entire library across all your devices —
+            with no paywalls, story limits or ads.
+          </p>
+        </div>
+        <hr className={styles.line} />
+        <div className={styles.innerdiv}>
+          <p className={styles.innerdivp1}>
+            Support the voices you want to hear more from.
+          </p>
+          <img
+            className={styles.innerdivimg}
+            src="https://cdn-static-1.medium.com/sites/medium.com/membership/images/SupportWriters.svg"
+            alt=""
+          />
+          <p className={styles.innerdivp2}>
+            A portion of your membership will directly support the writers and
+            thinkers you read the most.
+          </p>
+        </div>
+      </div>
+      <div className={styles.maindiv3}>
+        <p className={styles.maindiv3p1}>Why I'm a Medium Member...</p>
+      </div>
+      <div className={styles.maindiv4}>
+        <p>Take your mind</p>
+        <p>in new directions.</p>
+        <button className={styles.btn}>Become a member</button>
+        <img
+          src="https://cdn-static-1.medium.com/sites/medium.com/membership/images/Membership_Footer.svg"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
