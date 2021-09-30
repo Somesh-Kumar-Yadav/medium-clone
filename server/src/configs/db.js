24b519e887f55c67cf946322c260021c2af76962
Somesh <somesh_nj2_076@masai.school>
2021-09-30 13:42:27 +0530

    minor bugs

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -3,12 +3,15 @@ const mongoose = require("mongoose");
 const connect = () => {
 	//LOCALHOST=>  mongodb://localhost:27017/medium
 	//ATLAS=>  mongodb+srv://medium:medium@cluster0.vpk1n.mongodb.net/mediumClone?retryWrites=true&w=majority
-	return mongoose.connect("mongodb://localhost:27017/medium", {
-		useCreateIndex: true,
-		useFindAndModify: true,
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	return mongoose.connect(
+		"mongodb+srv://medium:medium@cluster0.vpk1n.mongodb.net/mediumClone?retryWrites=true&w=majority",
+		{
+			useCreateIndex: true,
+			useFindAndModify: true,
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	);
 };
 
 module.exports = connect;
