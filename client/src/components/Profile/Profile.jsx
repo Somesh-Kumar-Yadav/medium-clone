b02d636ee648f48aab7b28566c33ea8ee661f543
Somesh <somesh_nj2_076@masai.school>
2021-10-01 13:18:21 +0530

    minor bugs

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -4,9 +4,11 @@ import { GoogleLogout } from "react-google-login";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { logoutUser } from "../../redux/auth/actions";
+import { useHistory } from "react-router";
 
 const Profile = () => {
   const user = useSelector((state) => state.auth.user.user);
+  const history = useHistory();
   const dispatch = useDispatch();
   const logoutres = () => {
     alert("You have successfully Logged Out");
@@ -23,8 +25,8 @@ const Profile = () => {
             <h5>{user.name}</h5>
           </div>
           <hr className="line" />
-          <p className="links">Stories</p>
-          <p className="links">Write a Story</p>
+          <p className="links" onClick={()=>{history.push("/story")}}>Stories</p>
+          <p className="links" onClick={()=>{history.push("/newStory")}}>Write a Story</p>
           <div>
             <GoogleLogout
               clientId="378817930652-26drd8dhlmr4qet0ilu2qts92m12mpdr.apps.googleusercontent.com"
