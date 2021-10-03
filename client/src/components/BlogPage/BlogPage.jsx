d77f63b4a170050a472489951208cafc673aabbd
Somesh <somesh_nj2_076@masai.school>
2021-10-03 13:18:52 +0530

    Navbar added

--- client/src/components/BlogPage/BlogPage.jsx
+++ client/src/components/BlogPage/BlogPage.jsx
@@ -4,12 +4,15 @@ import LoginSignup from "../LoginSignup/LoginSignup";
 import { AboutAuthorWindow } from "./AboutAuthorWindow";
 import { BlogBody } from "./BlogBody";
 import { FollowNav } from "./FollowNav";
+import { NavBar } from "../LandingPage/NavBar";
+import { HomePageNavBar } from "../HomePage/HomePageNavBar";
 
 export const BlogPost = () => {
 
   useEffect(() => { }, []);
   const [open, setOpen] = useState(true);
   const data = useSelector((state) => state.auth.singleBlog);
+  const auth = Boolean(useSelector(state => state.auth.isAuth))
   const user = useSelector((state) => state.auth.user.user);
   if (!user) {
     return <LoginSignup status={open} set={setOpen} />;
@@ -17,6 +20,7 @@ export const BlogPost = () => {
 
   return (
     <div>
+      {auth ? <HomePageNavBar/>:<NavBar />}
       <FollowNav data={data}/>
       <AboutAuthorWindow data={data} />
       <BlogBody data={data} />
